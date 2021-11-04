82ffbbdfdef341896b818475a28f25a67602909d
Rus <de.mr44@gmail.com>
2021-11-04 19:54:33 +0500

    Initial Commit

---
+++ Multithreading-GCD5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreading-GCD5
+//
+//  Created by ruslan on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
